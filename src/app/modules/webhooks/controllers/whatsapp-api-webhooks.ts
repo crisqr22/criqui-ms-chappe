c246ccfdef2ece1143861d06e686c42f154105eb
Cristian Quintero <cristianqr22@gmail.com>
2023-03-04 19:42:12 -0500

    add scaffolding

--- src/app/modules/webhooks/controllers/whatsapp-api-webhooks.ts
+++ src/app/modules/webhooks/controllers/whatsapp-api-webhooks.ts
@@ -13,10 +13,14 @@ import { Response } from 'express';
 import { WhatsappAPIRequest } from '../interfaces/IApiWhatsApp';
 import { BuildMessage } from '../utils-functions/BuildMessage';
 import { Logger } from '../../../config/logger/logger.service';
+import { WhatsappApiService } from '../../../external-integrations/meta/services/whatsapp-api.service';
 
 @Controller(WebhooksRoutes.WHATSAPP_WEBHOOK)
 export class WhatsappApiWebhooks {
-  constructor(private readonly loggerService: Logger) {}
+  constructor(
+    private readonly loggerService: Logger,
+    private readonly whatsappApiService: WhatsappApiService,
+  ) {}
 
   @Get()
   public async verifyToken(
@@ -41,18 +45,33 @@ export class WhatsappApiWebhooks {
   }
 
   @Post()
-  public getMessage(@Res() res: Response, @Body() payload: WhatsappAPIRequest) {
+  public async getMessage(
+    @Res() res: Response,
+    @Body() payload: WhatsappAPIRequest,
+  ) {
     try {
-      this.loggerService.loggerInCloud(JSON.stringify(payload));
       let textMessage;
+      let traceabilityInternalData: any = {};
       const entry = payload.entry[0];
-      const changes = entry.changes[0];
-      if (changes.value.messages) {
-        const messageObject = changes.value.messages[0];
+      const change = entry.changes[0];
+      if (change.value.messages) {
+        const messageObject = change.value.messages[0];
         textMessage = BuildMessage(messageObject);
-      }
+        traceabilityInternalData.id = entry.id;
+        traceabilityInternalData.contact_name =
+          change.value.contacts[0].profile.name;
+        traceabilityInternalData.contact_phone = change.value.contacts[0].wa_id;
+        traceabilityInternalData.type = change.value.messages[0].type;
+        traceabilityInternalData.message = textMessage;
+        this.loggerService.loggerInCloud(
+          JSON.stringify(traceabilityInternalData),
+        );
 
-      this.loggerService.loggerInCloud(textMessage);
+        await this.whatsappApiService.sendPlainText(
+          traceabilityInternalData.contact_phone,
+          'Hola bienvenido a tu nuevo chat',
+        );
+      }
 
       return res.status(HttpStatus.OK).json({ textMessage });
     } catch (e) {
